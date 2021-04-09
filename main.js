2308f81729c59c66b90dc334408166e95d1bec7f
Ayaan-WHJ <78539077+Ayaan-WHJ@users.noreply.github.com>
2021-04-09 15:55:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,8 +16,24 @@ Webcam.snap(function(data_uri){
 }
 console.log('ml5 version:',ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/5HN9xUEC5/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/r-7uHfXst/model.json',modelLoaded);
 function modelLoaded()
 {
     console.log("modelLoaded");
+}
+function check()
+{
+img=document.getElementById('captured_image');
+classifier.classify(img, gotResult);
+}
+function gotResult(error, results)
+{
+    if(error) {
+console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
 }
